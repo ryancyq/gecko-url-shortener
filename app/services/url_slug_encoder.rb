96668b5697d4039268102028052d119e2ae77948
Ryan Chang <ryancyq@gmail.com>
2024-06-03 20:04:28 +0800

    fix: change slug encoder to convert input integer to string and extract binary data for encoding/decoding

--- app/services/url_slug_encoder.rb
+++ app/services/url_slug_encoder.rb
@@ -6,37 +6,66 @@ module UrlSlugEncoder
     result[char] = index
   end
   BASE = CHARS.length
+
   MAX_LENGTH = 15
   MAX_VALUE = (BASE**MAX_LENGTH) - 1
+  MAX_CHARS = (Math.log2(MAX_VALUE) / 8).floor
 
-  def self.encode(base_10_num)
-    raise ArgumentError, "Encode value can't be blank" unless base_10_num.present?
-    raise ArgumentError, "Encode value must be Integer" unless base_10_num.is_a?(Integer)
-    return nil if base_10_num < 0
-    return 0.to_s if base_10_num == 0
-    raise ArgumentError, "Encode value can't be greater than #{MAX_VALUE}" if base_10_num > MAX_VALUE
+  def self.encode(input)
+    intput = input.to_s if input.is_a?(Integer)
+
+    str = input&.to_s.presence
+    raise ArgumentError, "Encode string can't be blank" if str.blank?
+    raise ArgumentError, "Encode string can't be more than #{MAX_CHARS} chars" if str.length > MAX_CHARS
+
+    # read binary data from string in 8-bit unsigned slices
+    num = str.to_s.unpack("c*").each_with_index.reduce(0) do |result, (bits, pos)|
+      result + (bits << (8 * pos))
+    end
+
+    encode_int(num)
+  end
+
+  def self.encode_int(num)
+    raise ArgumentError, "Encode value must be Integer" unless num.is_a?(Integer)
+    return nil if num < 0
+    return 0.to_s if num == 0
+    raise ArgumentError, "Encode value can't be greater than #{MAX_VALUE}" if num > MAX_VALUE
 
     result = ""
-    while base_10_num > 0
-      result = CHARS[base_10_num % BASE] + result # working backwards from least significant part
-      base_10_num /= BASE
+    while num > 0
+      remainder = num % BASE
+      result = CHARS[remainder] + result # working backwards from least significant part
+      num -= remainder # ensure num is divisible by BASE
+      num /= BASE
     end
     result
   end
 
-  def self.decode(num_string)
-    text = num_string&.to_s.presence
-    raise ArgumentError, "Decode string can't be blank" if text.blank?
-    raise ArgumentError, "Decode string can't be more than #{MAX_LENGTH} chars" if num_string.length > MAX_LENGTH
+  def self.decode(input)
+    intput = input.to_s if input.is_a?(Integer)
 
+    str = input&.to_s.presence
+    raise ArgumentError, "Decode string can't be blank" if str.blank?
+    raise ArgumentError, "Decode string can't be more than #{MAX_LENGTH} chars" if str.length > MAX_LENGTH
+
+    # decode char from CHARS by summation
     i = 0
-    len = num_string.length - 1
-    result = 0
+    len = str.length - 1
+    num = 0
     while i <= len
       pow = BASE**(len - i)
-      result += CHARS_LOOKUP[num_string[i]] * pow
+      num += CHARS_LOOKUP[str[i]] * pow
       i += 1
     end
-    result
+
+    decode_int(num)
+  end
+
+  def self.decode_int(num)
+    raise ArgumentError, "Decode value must be Integer" unless num.is_a?(Integer)
+
+    length = (Math.log2(num) / 8).ceil
+    length.times.map { |pos| (num >> (8 * pos)) & 255 }.pack("C*")
   end
 end
--- spec/services/url_slug_encoder_spec.rb
+++ spec/services/url_slug_encoder_spec.rb
@@ -9,54 +9,68 @@ RSpec.describe UrlSlugEncoder do
     context "when value is nil" do
       let(:num) { nil }
 
-      it { expect { encoder }.to raise_error(ArgumentError, "Encode value can't be blank") }
+      it { expect { encoder }.to raise_error(ArgumentError, "Encode string can't be blank") }
     end
 
     context "when value is string" do
-      let(:num) { "1" }
+      context "with single digit" do
+        let(:num) { "1" }
 
-      it { expect { encoder }.to raise_error(ArgumentError, "Encode value must be Integer") }
+        it { is_expected.to eq "N" }
+      end
+
+      context "with double digits" do
+        let(:num) { "15" }
+
+        it { is_expected.to eq "3xD" }
+      end
+
+      context "with triple digits" do
+        let(:num) { "615" }
+
+        it { is_expected.to eq "eCRU" }
+      end
     end
 
     context "when value is negative" do
       let(:num) { -1 }
 
-      it { is_expected.to be_nil }
+      it { is_expected.to eq "3h3" }
     end
 
     context "when value is zero" do
-      let(:num) { -0 }
+      let(:num) { 0 }
 
-      it { is_expected.to eq "0" }
+      it { is_expected.to eq "M" }
     end
 
     context "when value is positive" do
       let(:num) { 1 }
 
-      it { is_expected.to eq "1" }
+      it { is_expected.to eq "N" }
     end
 
     context "when value is below MAX_VALUE" do
-      let(:num) { described_class::MAX_VALUE - 1 }
+      let(:num) { "a" * (described_class::MAX_CHARS - 1) }
 
-      it { is_expected.to eq "ZZZZZZZZZZZZZZY" }
+      it { is_expected.to eq "2ixmpKS37KxeTv" }
       it { expect(encoder.length).to be <= described_class::MAX_LENGTH }
     end
 
     context "when value is MAX_VALUE" do
-      let(:num) { described_class::MAX_VALUE }
+      let(:num) { "A" * described_class::MAX_CHARS }
 
-      it { is_expected.to eq "ZZZZZZZZZZZZZZZ" }
+      it { is_expected.to eq "6mnTu2VnVlbWXT3" }
       it { expect(encoder.length).to be <= described_class::MAX_LENGTH }
     end
 
     context "when value exceeds MAX_VALUE" do
-      let(:num) { described_class::MAX_VALUE + 1 }
+      let(:num) { "n" * (described_class::MAX_CHARS + 1) }
 
       it {
-        expect do
-          encoder
-        end.to raise_error(ArgumentError, "Encode value can't be greater than 768909704948766668552634367")
+        expect { encoder }.to raise_error(
+          ArgumentError, "Encode string can't be more than 11 chars"
+        )
       }
     end
   end
@@ -83,15 +97,15 @@ RSpec.describe UrlSlugEncoder do
     end
 
     context "when text is 1 char" do
-      let(:text) { "m" }
+      let(:text) { "Z" }
 
-      it { is_expected.to eq 22 }
+      it { is_expected.to eq "=" }
     end
 
-    context "when text is more than 1 char and less than 15 chars" do
-      let(:text) { "mBa2vcC9esdp51s" }
+    context "when text is more than 1 char and less than 16 chars" do
+      let(:text) { "5zQqItgWbaX7PyV" }
 
-      it { is_expected.to eq 280_272_376_445_206_540_999_443_446 }
+      it { is_expected.to eq "99999999999" }
     end
 
     context "when text is more than 15 chars" do
@@ -102,7 +116,7 @@ RSpec.describe UrlSlugEncoder do
   end
 
   context "when decode output is the same as encode input" do
-    let(:input) { 98_765 }
+    let(:input) { "98_765" }
 
     it "returns the same value" do
       encoded = described_class.encode(input)
