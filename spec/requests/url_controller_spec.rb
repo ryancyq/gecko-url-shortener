15041cc76d6ac323e133be71c3d8e2b06b21bc17
Ryan Chang <ryancyq@gmail.com>
2024-06-03 16:03:41 +0800

    spec: move url controller to request spec

--- spec/requests/url_controller_spec.rb
+++ spec/requests/url_controller_spec.rb
@@ -5,9 +5,9 @@ require "rails_helper"
 RSpec.describe UrlController do
   describe "GET root" do
     it "redirect to new_url_path" do
-      get :root
+      get "/"
 
-      expect(response).to redirect_to(new_url_path)
+      expect(response).to redirect_to("/url")
     end
   end
 
@@ -16,7 +16,7 @@ RSpec.describe UrlController do
     let(:short_url) { create(:short_url, target_url:) }
 
     it "redirect to target_url" do
-      get :redirect, params: { slug: short_url.slug }
+      get "/#{short_url.slug}"
 
       expect(response).to redirect_to(target_url.external_url)
     end
