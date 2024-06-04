02104affa7b44f0f407cd708ef58b26de0b96dd9
Ryan Chang <ryancyq@gmail.com>
2024-06-04 16:31:04 +0800

    spec: add url redirect with valid and invalid slug in request spec

--- spec/requests/url_controller_spec.rb
+++ spec/requests/url_controller_spec.rb
@@ -12,13 +12,23 @@ RSpec.describe UrlController do
   end
 
   describe "GET slug" do
-    let(:target_url) { create(:target_url) }
-    let(:short_url) { create(:short_url, target_url:) }
+    context "with existing slug" do
+      let(:target_url) { create(:target_url) }
+      let(:short_url) { create(:short_url, target_url:) }
 
-    it "redirect to target_url" do
-      get "/#{short_url.slug}"
+      it "redirect to target_url" do
+        get "/#{short_url.slug}"
 
-      expect(response).to redirect_to(target_url.external_url)
+        expect(response).to redirect_to(target_url.external_url)
+      end
+    end
+
+    context "with non-existing slug" do
+      it "redirect to url#new" do
+        get "/non-existing"
+
+        expect(response).to redirect_to(new_url_path)
+      end
     end
   end
 end
