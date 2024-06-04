95a2c4ac195465e763ef4385753f1b13433a8b30
Ryan Chang <ryancyq@gmail.com>
2024-06-04 22:09:28 +0800

    feat: implement geolocation API to ipinfo.io via geocoder gem

--- app/jobs/url_redirection/update_geo_location_job.rb
+++ app/jobs/url_redirection/update_geo_location_job.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "geocoder"
+
 class UrlRedirection::UpdateGeoLocationJob < ApplicationJob
   queue_as :default
 
@@ -11,5 +13,20 @@ class UrlRedirection::UpdateGeoLocationJob < ApplicationJob
       logger.warn { "UrlRedirectionEvent for short url id=#{short_url_id} not found" }
       return
     end
+
+    result, * = Geocoder.search(event.ip_address, ip_address: true)
+    unless result.present?
+      logger.info { "Geocoder returns empty result for #{event.ip_address}" }
+      return
+    end
+
+    update_params = {
+      country: result.country.presence,
+      city: result.city.presence
+    }.compact
+
+    logger.info { "Geocoder returns result without geolocation for #{event.ip_address}" } if update_params.blank?
+
+    event.update!(update_params) if update_params.key?(:country)
   end
 end
---
+++ spec/jobs/url_redirection/update_geo_location_job_spec.rb
@@ -0,0 +1,81 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+require "geocoder"
+
+RSpec.describe UrlRedirection::UpdateGeoLocationJob, type: :job do
+  let(:job) { described_class.new }
+
+  context "with non numeric as arg" do
+    it "does not invoke geocoder" do
+      expect(Geocoder).not_to receive(:search)
+      expect { job.perform "abc" }.not_to raise_error
+    end
+  end
+
+  context "with non-existing id as arg" do
+    it "does not invoke geocoder" do
+      expect(Geocoder).not_to receive(:search)
+      expect { job.perform "9999" }.not_to raise_error
+    end
+  end
+
+  context "with existing id as arg" do
+    let(:short_url) { build(:short_url) }
+    let(:url_redirection_event) do
+      create(:url_redirection_event, short_url:, ip_address:, country:, city:) 
+    end
+    let(:country) { "Singapore" }
+    let(:city) { "Singapore" }
+
+    context "with public ip address" do
+      let(:ip_address) { "66.131.120.255" }
+
+      it "invokes geocoder", vcr: "url_redirection/geolocation_with_public_ip_address" do
+        expect(Geocoder).to receive(:search).and_call_original
+        expect { job.perform url_redirection_event.short_url.id }.not_to raise_error
+      end
+
+      it "updates country and city of the event", vcr: "url_redirection/geolocation_with_public_ip_address" do
+        job.perform url_redirection_event.short_url.id
+
+        url_redirection_event.reload
+        expect(url_redirection_event.country).to eq "CA"
+        expect(url_redirection_event.city).to eq "Dolbeau-Mistassini"
+      end
+
+      it "does not update if data absent", vcr: "url_redirection/geolocation_with_public_ip_address_missing" do
+        job.perform url_redirection_event.short_url.id
+
+        url_redirection_event.reload
+        expect(url_redirection_event.country).to eq "Singapore"
+        expect(url_redirection_event.city).to eq "Singapore"
+      end
+
+      it "does not update if country is absent and city is present", vcr: "url_redirection/geolocation_with_public_ip_address_partial" do
+        job.perform url_redirection_event.short_url.id
+
+        url_redirection_event.reload
+        expect(url_redirection_event.country).to eq "Singapore"
+        expect(url_redirection_event.city).to eq "Singapore"
+      end
+    end
+
+    context "with internal ip address" do
+      let(:ip_address) { "250.222.51.205" }
+
+      it "invokes geocoder", vcr: "url_redirection/geolocation_with_internal_ip_address" do
+        expect(Geocoder).to receive(:search).and_call_original
+        expect { job.perform url_redirection_event.short_url.id }.not_to raise_error
+      end
+
+      it "does not update if data absent", vcr: "url_redirection/geolocation_with_internal_ip_address" do
+        job.perform url_redirection_event.short_url.id
+
+        url_redirection_event.reload
+        expect(url_redirection_event.country).to eq "Singapore"
+        expect(url_redirection_event.city).to eq "Singapore"
+      end
+    end
+  end
+end
