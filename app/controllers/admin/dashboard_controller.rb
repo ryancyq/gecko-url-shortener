fedaf0e326b2f1579e3b97aadc0ed3b53768abd9
Ryan Chang <ryancyq@gmail.com>
2024-06-05 16:03:31 +0800

    fix: add admin layout with different link for application name

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Admin::DashboardController < ApplicationController
+  layout "admin"
+
   def index
     @target_urls = TargetUrl.order(created_at: :desc).all
   end
