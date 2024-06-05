4e12f3a7ed814a3f6b62d41a042870ebb5020f3e
Ryan Chang <ryancyq@gmail.com>
2024-06-05 16:01:24 +0800

    feat: implement target url listings

--- spec/system/admin/dashboard_system_spec.rb
+++ spec/system/admin/dashboard_system_spec.rb
@@ -15,4 +15,25 @@ RSpec.describe "Admin Dashbaord" do
       expect(page).to have_text("There are no URLs being shortened.")
     end
   end
+
+  context "with shorten URLs" do
+    let(:target_url) { build(:target_url) }
+    let(:another_target_url) { build(:target_url) }
+
+    before do
+      create(:short_url, target_url:)
+      create(:short_url, target_url: another_target_url)
+    end
+
+    it "shows target urls" do
+      visit "/admin"
+
+      expect(page).to have_text("Admin")
+      expect(page).to have_text("Target URLs")
+      expect(page).to have_text(target_url.title)
+      expect(page).to have_link(target_url.external_url, href: target_url.external_url)
+      expect(page).to have_text(another_target_url.title)
+      expect(page).to have_link(another_target_url.external_url, href: another_target_url.external_url)
+    end
+  end
 end
